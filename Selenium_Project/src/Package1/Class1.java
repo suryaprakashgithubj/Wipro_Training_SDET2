e7236477e33d9bc7965603de777a9f43b67267a4
suryaprakashj <suryaprakashj@172.20.10.3>
2026-04-21 10:43:13 +0530

    new branch

--- Selenium_Project/src/Package1/Class1.java
+++
@@ -1,8 +0,0 @@
-package Package1;
-
-public class Class1 {
-    public static void main(String[] args)
-    {
-    	System.out.print("Hi");
-    }
-}
---
+++ Selenium_Project/src/Package1/test_newbranch.java
@@ -0,0 +1,8 @@
+package Package1;
+
+public class test_newbranch {
+	public static void main(String[] args)
+    {
+    	System.out.print("Testing new branch");
+    }
+}

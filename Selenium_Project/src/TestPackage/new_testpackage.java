b0523eb6e7c870270f8fafcc1d8355a0f28e8388
suryaprakashj <suryaprakashj@172.20.10.3>
2026-04-21 13:01:13 +0530

    i need to add new package

---
+++ Selenium_Project/src/TestPackage/new_testpackage.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+public class new_testpackage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("testing the new package");
+	}
+
+}
